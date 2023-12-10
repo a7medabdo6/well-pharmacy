16c89d7275e5483071369d7c26fdc4ed01ff5aab
a7medabdo6 <57114105+a7medabdo6@users.noreply.github.com>
2023-12-10 22:26:20 +0200

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+    <link rel="shortcut icon" href="/static/favicon.ico" />
+    https://well-medic.com/categories/22
+    </Head>
       <body>
         <Main />
         <NextScript />
